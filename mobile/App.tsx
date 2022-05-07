17af2028c0a944c98b6cf4264d0edd498170c83e
rafaelreisramos <rrramos@gmail.com>
2022-05-07 12:17:20 -0300

    fix: types conflict
    - add react-dom types to avoid monorepo conflicts
    - replace deprecated app-loading with new splash-screen for loading fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,8 @@ import 'react-native-gesture-handler'
 
 import { View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import AppLoading from 'expo-app-loading'
+import * as SplashScreen from 'expo-splash-screen'
+
 import {
   useFonts,
   Inter_400Regular,
@@ -13,14 +14,17 @@ import Widget from './src/components/Widget'
 import { theme } from './src/theme'
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync()
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium
   })
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return null
   }
+
+  SplashScreen.hideAsync()
 
   return (
     <View
