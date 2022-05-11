0d94b2b474f3d85fe161ae565f98796d1f5b803c
rafaelreisramos <rrramos@gmail.com>
2022-05-11 14:44:58 -0300

    deploy(railway): add PORT env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-const PORT = process.env.HTTP_PORT || 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
