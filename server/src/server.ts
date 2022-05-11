23577fe053e72365d04c932140111606bcfcce47
rafaelreisramos <rrramos@gmail.com>
2022-05-11 20:45:01 -0300

    fix: increase limit for payloads

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import { routes } from './routes'
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '1mb' }))
 app.use(routes)
 
 const PORT = process.env.PORT || 3333
